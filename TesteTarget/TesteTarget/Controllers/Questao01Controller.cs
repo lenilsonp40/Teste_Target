5c1ef13302f9cc4cd51c6889219438e82b494f07
Lenilson Soares <lenilsonp40@gmail.com>
2024-09-03 09:47:04 -0300

    teste target

---
+++ TesteTarget/TesteTarget/Controllers/Questao01Controller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TesteTarget.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FibonacciController : ControllerBase
+    {
+        
+        [HttpGet("{number}")]
+        public IActionResult Get(int number)
+        {
+            bool isFibonacci = IsInFibonacciSequence(number);
+
+            if (isFibonacci)
+            {
+                return Ok(new { Message = $"{number} pertence a sequência de Fibonacci." });
+            }
+            else
+            {
+                return Ok(new { Message = $"{number} não pertence a sequência de Fibonacci." });
+            }
+        }
+
+        private bool IsInFibonacciSequence(int number)
+        {
+            if (number < 0) return false;
+
+            int previous = 0;
+            int current = 1;
+
+            while (current <= number)
+            {
+                if (current == number) return true;
+
+                int next = previous + current;
+                previous = current;
+                current = next;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TesteTarget/TesteTarget/Controllers/Questao02MaisculaOuMinusculaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StringAnalysisApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Questao02MaisculaOuMinusculaController : ControllerBase
+    {
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString))
+            {
+                return BadRequest(new { Message = "A string fornecida está vazia ou nula." });
+            }
+
+            int count = CountLetterA(inputString);
+
+            if (count > 0)
+            {
+                return Ok(new { Message = $"A letra 'a' ocorre {count} vezes na string." });
+            }
+            else
+            {
+                return Ok(new { Message = "A letra 'a' não está presente na string." });
+            }
+        }
+
+        private int CountLetterA(string input)
+        {
+            int count = 0;
+            foreach (char c in input)
+            {
+                if (char.ToLower(c) == 'a')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
